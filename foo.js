4073264d7d0af426bf69940787f208005861de33
Matthew Knox <mrkno@users.noreply.github.com>
2016-09-08 10:41:24 +1200

    Update foo.js

--- foo.js
+++ foo.js
@@ -2,15 +2,15 @@ let acronym = require('acronym-maker');
 
 exports.run = (api, event) => {
     if (event.arguments.length !== 2) {
-        return api.sendMessage("WTF are you doing?", event.thead_id);
+        return api.sendMessage("WTF are you doing?", event.thread_id);
     }
 
     let ac = event.arguments[1];
     acronym.create(ac, (err, res) => {
         if (err) {
-            return api.sendMessage("Something went wrong. Its probably your fault " + event.sender_name, event.thead_id);
+            return api.sendMessage("Something went wrong. Its probably your fault " + event.sender_name, event.thread_id);
         }
         let msg = res.join(' ');
-        api.sendMessage(msg, event.thead_id);
+        api.sendMessage(msg, event.thread_id);
     });
 };

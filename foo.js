e98086a0b505dc81083fa2f6ed65014ee6b0849f
Matthew Knox <mrk45@uclive.ac.nz>
2016-09-08 11:37:09 +1200

    Punctuation

--- foo.js
+++ foo.js
@@ -8,7 +8,7 @@ exports.run = (api, event) => {
 
     let ac = event.arguments[1];
 	if (ac.length >= 3 && ac.length <= 15 && api.random([true, false]) && /[a-zA-Z]/.test(ac)) {
-		// method 1 - will probably produce better acronyms but has length limits and cant deal with numbers
+		// method 1 - will probably produce better acronyms but has length limits and cant deal with numbers/punctuation
 		maker.create(ac, (err, res) => {
 			if (err) {
 				return api.sendMessage("Something went wrong. Its probably your fault " + event.sender_name, event.thread_id);

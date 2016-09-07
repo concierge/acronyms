e72fe9a3a442bb44ae3440f30742d10ea5d0fcc4
Matthew Knox <mrk45@uclive.ac.nz>
2016-09-08 11:09:22 +1200

    DC

--- foo.js
+++ foo.js
@@ -1,4 +1,5 @@
-let acronym = require('acronym-maker');
+let maker = require('acronym-maker'),
+	generator = require('acronym');
 
 exports.run = (api, event) => {
     if (event.arguments.length !== 2) {
@@ -6,11 +7,18 @@ exports.run = (api, event) => {
     }
 
     let ac = event.arguments[1];
-    acronym.create(ac, (err, res) => {
-        if (err) {
-            return api.sendMessage("Something went wrong. Its probably your fault " + event.sender_name, event.thead_id);
-        }
-        let msg = res.join(' ');
-        api.sendMessage(msg, event.thead_id);
-    });
+	if (ac.length >= 3 && ac.length <= 15 && api.random([true, false])) {
+		// method 1
+		maker.create(ac, (err, res) => {
+			if (err) {
+				return api.sendMessage("Something went wrong. Its probably your fault " + event.sender_name, event.thead_id);
+			}
+			let msg = res.join(' ');
+			api.sendMessage(msg, event.thead_id);
+		});
+	}
+	else {
+		let res = generator(ac);
+		api.sendMessage(res, event.thead_id);
+	}
 };
